c45ca1d68b8da606447ec932b09ded7ea0bfd511
ishaghodgaonkar <ighodgao@purdue.edu>
2020-04-19 10:54:50 -0400

    Revert "updated network" never mind This reverts commit 8085129a50a82d11978cdbbc6ce832c7afac884a.

--- mixnet.py
+++ mixnet.py
@@ -6,21 +6,16 @@ class mixBlock(nn.Module):
     """For layers V1 through IT"""
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=stride, padding=kernel_size//2)
+        self.conv = nn.Conv2d(in_channels, in_channels, kernel_size = kernel_size, stride=stride, padding=1)
         self.nonlin = nn.ReLU(inplace=True)
         self.pool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-        # self.dimred = nn.Conv2d(in_channels*2, out_channels, kernel_size=1, stride=1, padding=0)
-
-
+        self.dimred = nn.Conv2d(in_channels*2, out_channels, kernel_size=1, stride=1, padding=0)
     def forward(self, input):
         x = self.conv(input)
         x = self.nonlin(x)
         x = self.pool(x)
-
-        # implementing skip connection, similar to RESnet, by preserving input information
         x = torch.cat((x, input), 1)
-
-        # x = self.dimred(x)
+        x = self.dimred(x)
         return x
 
 
@@ -29,18 +24,14 @@ class Retina(nn.Module):
         super().__init__()
 
         """
-        
         Horizontal cells activate the bipolar cell layer using lateral inhibition. This effectively increases 
         contrast at edges. To model this, the kernel learned in the bipolar convolutional layer should be some type of 
-        edge detection or sharpen kernel. Initialize weights as that kernel, and they will adjust from there.
+        edge detection or sharpen kernel. 
         """
         weights = torch.tensor([[-1., -1., -1.],
                                 [-1., 8., -1.],
                                 [-1., -1., -1.]])
-
-        # reshape and copy for 16 channel output
         weights = weights.view(1, 1, 3, 3).repeat(16, 1, 1, 1)
-
         self.bipolar_conv = nn.Conv2d(in_channels, 16, kernel_size =3, stride=1, bias=False)
         self.bipolar_conv.weight = nn.Parameter(weights)
 
@@ -52,19 +43,19 @@ class Retina(nn.Module):
         
         """
 
-        self.ganglion_conv = nn.Conv2d(16, 32, kernel_size=5, stride=1)
+        self.ganglion_conv = nn.Conv2d(16, 24, kernel_size=5, stride=1)
 
         """
-        Expect to see similar receptive fields as ganglion cells (circular, activated in the center or in periphery). 
+        Similar receptive fields as ganglion cells (circular, activated in the center or in periphery). 
         """
 
-        self.ganglion_conv2 = nn.Conv2d(32, out_channels, kernel_size=3, stride=1)
+        self.LGN_conv = nn.Conv2d(24, out_channels, kernel_size=3, stride=1)
 
     def forward(self, input):
 
         x = self.bipolar_conv(input)
         x = self.ganglion_conv(x)
-        x = self.ganglion_conv2(x)
+        x = self.LGN_conv(x)
 
         return x
 
@@ -77,16 +68,17 @@ class Flatten(nn.Module):
 def mixnetV1():
     model = nn.Sequential(
         OrderedDict([
-            # Images are grey-scaled to result in one input channel
-            ('Retina', Retina(1, 64)),
-            ('V1', mixBlock(64, 128, kernel_size=5)),
-            ('V2', mixBlock(128, 256)),
-            ('V4', mixBlock(256, 512)),
-            ('IT', mixBlock(512, 1024)),
+            ('Retina', Retina(1, 32)),
+            ('V1', mixBlock(32, 48)),
+            ('V2', mixBlock(48, 64)),
+            ('V3', mixBlock(64, 96)),
+            ('V4', mixBlock(96, 128)),
+            ('IT', mixBlock(128, 192)),
             ('decoder', nn.Sequential(OrderedDict([
                 ('avgpool', nn.AdaptiveAvgPool2d(1)),
                 ('flatten', Flatten()),
-                ('linear1', nn.Linear(1024, 1000)),
+                ('linear1', nn.Linear(192, 4096)),
+                ('linear2', nn.Linear(4096, 1000))
             ])))
         ]))
     return model
